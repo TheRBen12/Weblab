d6566d5f8909ceecc604a9013ef5c75fddb1b13f
romanobrentani <brentani.v@gmail.com>
2025-02-19 21:32:48 +0100

    implemented experiment test controleller

--- Controllers/ExperimentController.cs
+++ Controllers/ExperimentController.cs
@@ -5,15 +5,30 @@ using WebLab.Models;
 
 namespace WebLab.Controllers;
 
-public class ExperimentController(ApplicationDbContext context): BaseController
+public class ExperimentController(ApplicationDbContext context) : BaseController
 {
     public readonly ApplicationDbContext _context = context;
-    
-    
+
+
     [HttpGet]
     public async Task<ActionResult<List<Experiment>>> GetExperiments()
     {
         var experiments = await context.Experiments.ToListAsync();
         return experiments;
+    }
+
+    [HttpGet("{experimentId}")]
+    public async Task<ActionResult<Experiment>> FindExperiment(int experimentId)
+    {
+        var experiment = await context.Experiments.FindAsync(experimentId);
+        if (experiment != null)
+        {
+            return experiment;
+        }
+
+        {
+            return NotFound("Das Experiment mit der Id " + experimentId + " wurde nicht gefunden");
+        }
+        return experiment;
     }
 }
--- Controllers/ExperimentTestController.cs
+++ Controllers/ExperimentTestController.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using WebLab.Data;
+using WebLab.Models;
 
 namespace WebLab.Controllers;
 
 public class ExperimentTestController(ApplicationDbContext context): BaseController
 {
-    
+    public readonly ApplicationDbContext _context = context;
+
+    public async Task<IEnumerable<ExperimentTest>> GetExperimentTests(int experimentId)
+    {
+        var experimentTests =  await _context.ExperimentTests
+            .Where(expTest => expTest.Experiment.Id == experimentId)
+            .ToListAsync();
+        return experimentTests;
+    }
+
 }
